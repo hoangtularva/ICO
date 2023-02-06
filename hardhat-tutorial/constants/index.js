da3865a566bfe65bfccf7a73ea994df696b59f71
Tran Hoang Tu <50492205+hoangtularva@users.noreply.github.com>
2023-02-06 17:16:26 +0700

    Launch your own Initial Coin Offering

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xA5991A7f5753F5Ff9b100fDfb29a0D72A4540536";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
